c42ac1e48167c6ce133e28be3e01b8f4ec61ffb2
gowthamtrm01 <74961233+gowthamtrm01@users.noreply.github.com>
2021-04-21 13:47:16 +0530

    Add files via upload

---
+++ Task-02-script.js
@@ -0,0 +1,10 @@
+var req = new XMLHttpRequest();
+req.open('GET', 'https://restcountries.eu/rest/v2/all', true);
+req.send();
+req.onload = function() {
+    var data = JSON.parse(this.response);
+    for(const country of data ){
+        console.log(`Country Name: ${country.name}`);
+    }
+    
+}   
